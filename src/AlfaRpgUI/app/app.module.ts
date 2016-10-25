38c4ae52adcd66bdd93dda7cea4d17e8418523bd
Blazej Pietrzyk <b.pietrzyk@alfavox.pl>
2016-10-25 17:35:14 +0200

    [*][bp] Full tutorial done

--- src/AlfaRpgUI/app/app.module.ts
+++ src/AlfaRpgUI/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { HeroService } from './services/hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './services/in-memory-data.service';
+
+import './rxjs-extensions';
 
 @NgModule({
     imports: [ 
@@ -27,7 +30,8 @@ import { InMemoryDataService }  from './services/in-memory-data.service';
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     providers: [ HeroService ],
     bootstrap: [ AppComponent ]
