1aaf4a55eff433d3330ddbdfd49cc030a65970b6
Blazej Pietrzyk <blazej_pietrzyk@o2.pl>
2016-11-03 08:03:01 +0100

    [*][bp] WebPack integration - not finished

---
+++ src/AlfaRPG/src/app/vendor.ts
@@ -0,0 +1,27 @@
+// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+
+import 'primeng/primeng';
+
+import 'primeng/resources/primeng.min.css';
+import 'primeng/resources/themes/omega/theme.css';
+import '../styles.css';
+
